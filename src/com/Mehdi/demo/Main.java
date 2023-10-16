4ef98718f1016e7072adec28deebe661c640f81e
mehdinoori20 <mehdi.noori1994@hotmail.com>
2023-10-16 20:46:04 +0200

    first commit

---
+++ src/com/Mehdi/demo/Main.java
@@ -0,0 +1,17 @@
+package com.Mehdi.demo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What's your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Player1: " + name);
+
+
+
+    }
+
+}
